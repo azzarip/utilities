5a7ac27e485dbfd88f2f4368ef2f0fa7be3acb9f
Azzarip <paride21azzari@gmail.com>
2024-04-13 19:07:18 +0200

    fixed all tests

--- tests/AdminPanel/PanelPagesTest.php
+++ tests/AdminPanel/PanelPagesTest.php
@@ -15,3 +15,14 @@ it('redirects to login', function () {
     get(route('admin.dashboard'))->assertRedirectToRoute('login');
 });
 
+it('returns other panels', function () {
+    $this->artisan('admin-panel:make test');
+    $this->refreshApplication();
+    $this->actingAs($this->user);
+    $this->withoutVite();
+    get('http://admin.localhost.test/test')->assertOk();
+});
+
+it('404 on other pages', function () {
+    get('http://admin.localhost.test/paco')->assertStatus(404);
+});
