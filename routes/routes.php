d10d6fd37084297b854176280fc6c8b5931b67d8
Azzarip <paride21azzari@gmail.com>
2024-04-13 16:59:47 +0200

    renamed route file

---
+++ routes/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+use Azzarip\Utilities\AdminPanel\AdminPanel;
+use Illuminate\Support\Facades\Route;
+use Azzarip\Utilities\Http\Controllers\DeployController;
+use Azzarip\Utilities\CookieConsent\Middleware\AuthenticateSession;
+
+Route::view('/privacy-policy', 'Azzarip::privacy')->name('privacy-policy');
+Route::view('/cookie-policy', 'Azzarip::cookie')->name('cookie-policy');
+
+//Route::post('/deploy', DeployController::class);
+
+Route::domain('admin.' . config('domains.base'))
+    ->middleware(['web', 'auth', AuthenticateSession::class])
+    ->group(function () {
+        Route::view('/', 'azzarip::main')->name('admin.dashboard');
+        Route::get('/{panel}', function (string $panel) {
+            return view('azzarip::' . $panel);
+        })->whereIn('panel', array_keys(AdminPanel::items()))
+        ->name('admin');
+    });
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Azzarip\Utilities\Http\Controllers\DeployController;
-
-Route::view('/privacy-policy', 'Azzarip::privacy')->name('privacy-policy');
-Route::view('/cookie-policy', 'Azzarip::cookie')->name('cookie-policy');
-
-Route::post('/deploy', DeployController::class);
