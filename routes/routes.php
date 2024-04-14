bcf09de67938d34e042adad65a32b677236391dd
Azzarip <paride21azzari@gmail.com>
2024-04-14 16:43:56 +0200

    fixed errors

--- routes/routes.php
+++ routes/routes.php
@@ -7,14 +7,16 @@ use Azzarip\Utilities\CookieConsent\Middleware\AuthenticateSession;
 
 Route::view('/privacy-policy', 'azzarip::privacy')->name('privacy-policy');
 Route::view('/cookie-policy', 'azzarip::cookie')->name('cookie-policy');
-//Route::post('/deploy', DeployController::class);
+Route::post('/deploy', DeployController::class);
 
 Route::domain('admin.' . config('domains.base'))
     ->middleware(['web', 'auth', 'verified', AuthenticateSession::class])
     ->group(function () {
         Route::view('/', 'vendor.admin-panel.home')->name('admin.dashboard');
+    If( ! empty(AdminPanel::items())){
         Route::get('/{panel}', function (string $panel) {
             return view('vendor.admin-panel.' . $panel);
         })->whereIn('panel', array_keys(AdminPanel::items()))
         ->name('admin');
+    }
     });
