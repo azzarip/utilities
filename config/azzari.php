50aaa7f03ac531a897d1d77afcf889afefa5a146
Azzarip <paride21azzari@gmail.com>
2024-04-24 17:39:56 +0200

    it has middleware for api

---
+++ config/azzari.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'call' => [
+        'username' => env('APP_NAME'),
+        'password' => env('AZZARI_CALL_PASSWORD'),
+        'url' => rtrim(env('AZZARI_CALL_URL'), '/'),
+    ],
+
+    'response' => [
+        'username' => 'pizzaazzari',
+        'password' => env('AZZARI_RESPONSE_PASSWORD'),
+    ],
+];
---
+++ src/Http/Middleware/AuthorizeAzzariApi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizeAzzariApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $username = $request->getUser();
+        $password = $request->getPassword();
+        if ($username != config('azzari.response.username') || $password != config('azzari.response.password')) {
+            return response()->json(['error' => 'Authentication Error.'], 401);
+        }
+        return $next($request);
+    }
+}
