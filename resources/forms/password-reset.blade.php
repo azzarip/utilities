2bcda817b86cd65b6cc4b50538b4623e5f402bb4
Azzarip <paride21azzari@gmail.com>
2024-08-21 17:13:02 +0200

    fixed

--- resources/forms/password-reset.blade.php
+++ resources/forms/password-reset.blade.php
@@ -1,7 +1,7 @@
 <div>
 <h1 style="margin-bottom: 1rem; font-size: 1.875rem; line-height: 1.5; text-align: center; color: #000000;">@lang('a::auth.title.reset')</h1>
 
-<x-forms::base :action="route('password.reset')" :button="__('a::auth.message.reset')" >
+<x-forms::base :action="route('password.reset')" :button="__('a::auth.reset_password')" >
 
     <p>@lang('a::auth.message.reset')</p>
 
