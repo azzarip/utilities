5833804428524e8a07a783190af44a811a21f445
Azzarip <paride21azzari@gmail.com>
2024-08-25 16:14:59 +0200

    added german

---
+++ resources/lang/de/forms.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'label' => [
+        'phone' => 'Handynummer',
+        'first_name' => 'Vorname',
+        'last_name' => 'Nachname',
+    ],
+    'placeholder' => [
+        'email' => 'Deine Email',
+        'first_name' => 'Dein Vorname',
+        'last_name' => 'Dein Nachname',
+        'new-password' => 'Dein neue Password',
+        'current-password' => 'Dein Password',
+    ],
+    'privacy_policy' => 'I consent to the terms of the :link',
+];
