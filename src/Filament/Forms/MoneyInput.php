0c8ad4d4dfd099073983ce5b5346ab564a3a0d41
Azzarip <paride21azzari@gmail.com>
2024-09-05 11:57:01 +0200

    moneyinput

---
+++ src/Filament/Forms/MoneyInput.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Azzarip\Utilities\Filament\Forms;
+
+use Filament\Forms\Components\TextInput;
+
+class MoneyInput
+{
+
+    public static function make(string $name)
+    {
+        return TextInput::make($name)
+        ->required()
+        ->numeric()
+        ->minValue(0)
+        ->inputMode('decimal')
+        ->step(0.05)
+        ->suffix('CHF');
+    }
+}
