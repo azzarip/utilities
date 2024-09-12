b7379d8558f90e9ebbce86be32508df88c2f0dcc
Azzarip <paride21azzari@gmail.com>
2024-09-12 19:18:46 +0200

    button is optional

--- resources/views/forms/base.blade.php
+++ resources/views/forms/base.blade.php
@@ -1,4 +1,4 @@
-@props(['button', 'action' => null, 'id' => null])
+@props(['button' => false, 'action' => null, 'id' => null])
 <form
 @if($action) action="{{ $action }}" @endif
 @if($id) id="{{ $id }}" @endif
@@ -7,6 +7,8 @@ method="POST">
     <div class="space-y-4">
         {{ $slot }}
     </div>
+    @if($button)
     <x-button type="submit">{{ $button }}</x-button>
+    @endif
 
 </form>
