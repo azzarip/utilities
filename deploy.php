3edd576cfbc2175545673acd14eed0b66c00c624
Azzarip <paride21azzari@gmail.com>
2024-09-18 11:44:17 +0200

    it has deployer

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+set('repository', 'git@gh-{{key}}:azzarip/{{key}}.git');
+set('keep_releases', 3);
+
+
+// Hosts
+host('pizza-azzari-deployer')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '/var/www/{{key}}')
+    ->set('ssh_multiplexing', false);
+
+
+desc('It generates the sitemaps');
+task('artisan:sitemap:generate', artisan('sitemap:generate'));
+
+desc('It generates the sitemaps');
+task('deploy:build', function () {
+    run('cd {{release_path}} && npm link vite');
+    run('cd {{release_path}} && npm link laravel-vite-plugin');
+    run('cd {{release_path}} && npm run build');
+});
+
+desc('Clears all compiled view files');
+task('artisan:optimize', artisan('optimize'));
+
+after('deploy:symlink', 'deploy:build');
+after('deploy:failed', 'deploy:unlock');
+
+task('test:namespace', function () {
+    writeln('It is pulling the tasks from Utilities');
+});
+
+desc('Deploys your project');
+task('deploy', [
+        'deploy:prepare',
+        'deploy:vendors',
+        'artisan:storage:link',
+        'artisan:optimize',
+        'artisan:migrate',
+        'deploy:publish',
+]);
