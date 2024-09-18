db6cae3442308bd2dc4872e4ce8749a94d7693ab
Azzarip <paride21azzari@gmail.com>
2024-09-18 16:28:05 +0200

    has back menu

---
+++ src/Filament/Items/BackMain.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Azzarip\Utilities\Filament\Items;
+
+use Filament\Navigation\NavigationItem;
+
+class BackMain {
+
+    public static function make() {
+        return NavigationItem::make('Back to Main Menu')
+        ->url('/')
+        ->icon('heroicon-o-arrow-uturn-left')
+        ->sort(-9999);
+    }
+}
