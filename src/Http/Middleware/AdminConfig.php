71a29cf49d642c756ee100bf0c05b8d544a6880d
Azzarip <paride21azzari@gmail.com>
2025-02-09 19:04:05 +0100

    has admin config

---
+++ src/Http/Middleware/AdminConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Azzarip\Utilities\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminConfig
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Config::set('app.locale', 'en');
+        Config::set('app.debug', true);
+        return $next($request);
+    }
+}
